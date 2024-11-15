b172cef5794c4bc364abcb389f858de48eeb20bb
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-11-15 17:44:20 +0530

    added homepage

--- src/my-org-root-config.js
+++ src/my-org-root-config.js
@@ -3,11 +3,51 @@ import { registerApplication, start } from "single-spa";
 
 
 registerApplication({
+  name: "@my-org/navbar",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@my-org/navbar"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@my-org/styleguide",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@my-org/styleguide"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
   name: "@my-org/product",
   app: () =>
     import(
       /* webpackIgnore: true */
       "@my-org/product"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@my-org/home",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@my-org/home"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@my-org/user",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@my-org/user"
     ),
   activeWhen: ["/"],
 });
